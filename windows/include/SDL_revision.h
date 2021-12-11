b6d497c9c2e0bb7da98519ea45e5c75bc19b6451
Cameron Gutman <aicommander@gmail.com>
2021-12-11 14:00:05 -0600

    Update SDL and switch to arch-specific headers on Windows
    SDL -> e2ade2b

--- windows/include/SDL_revision.h
+++
@@ -1,6 +0,0 @@
-#define SDL_REVISION "https://github.com/libsdl-org/SDL.git@7f308abb559e426ec7562a47db5176bb11a9175d"
-#define SDL_REVISION_NUMBER 0
-
-#ifndef SDL_REVISION
-#define SDL_REVISION ""
-#endif
---
+++ windows/include/arm64/SDL_revision.h
@@ -0,0 +1,6 @@
+#define SDL_REVISION "https://github.com/libsdl-org/SDL.git@e2ade2bfc46d915cd306c63c830b81d800b2575f"
+#define SDL_REVISION_NUMBER 0
+
+#ifndef SDL_REVISION
+#define SDL_REVISION ""
+#endif
