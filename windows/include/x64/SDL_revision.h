7dfbd3eda16dd072799c5903f747c57804e4714a
Cameron Gutman <aicommander@gmail.com>
2022-03-30 23:27:00 -0500

    Update SDL, SDL_ttf, and FreeType
    SDL -> 2c04df8
    SDL_ttf -> 80d909 (Windows)
    FreeType -> 3f83da (Windows)

--- windows/include/x64/SDL_revision.h
+++ windows/include/x64/SDL_revision.h
@@ -1,4 +1,4 @@
-#define SDL_REVISION "https://github.com/libsdl-org/SDL.git@cbdb67b4f655000de35acb5629ddb08cc6761e8a"
+#define SDL_REVISION "https://github.com/libsdl-org/SDL.git@2c04df8d651ca6ec7425f49621c1e55144769cc9"
 #define SDL_REVISION_NUMBER 0
 
 #ifndef SDL_REVISION
