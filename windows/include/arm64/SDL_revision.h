75131039a78fc20867b3508c0111163eea2c1463
Cameron Gutman <aicommander@gmail.com>
2023-07-09 17:41:33 -0500

    Update libs
    OpenSSL -> 1.1.1u
    SDL -> 156c7ba

--- windows/include/arm64/SDL_revision.h
+++ windows/include/arm64/SDL_revision.h
@@ -2,7 +2,7 @@
 #define SDL_REVISION_NUMBER 0
 
 #ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL-release-2.26.0-306-gb57a6a1ad (" SDL_VENDOR_INFO ")"
+#define SDL_REVISION "SDL-release-2.28.0-47-g156c7badf (" SDL_VENDOR_INFO ")"
 #else
-#define SDL_REVISION "SDL-release-2.26.0-306-gb57a6a1ad"
+#define SDL_REVISION "SDL-release-2.28.0-47-g156c7badf"
 #endif
