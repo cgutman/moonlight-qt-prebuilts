6f73f49f2880304ea913bda9e3c2a89735cb05a8
Cameron Gutman <aicommander@gmail.com>
2023-10-18 00:39:46 -0500

    Update SDL2 to 4aab234

--- windows/include/x64/SDL_revision.h
+++ windows/include/x64/SDL_revision.h
@@ -2,7 +2,7 @@
 #define SDL_REVISION_NUMBER 0
 
 #ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL-release-2.28.0-128-gf032e8c19 (" SDL_VENDOR_INFO ")"
+#define SDL_REVISION "SDL-release-2.28.0-178-g4aab2342e (" SDL_VENDOR_INFO ")"
 #else
-#define SDL_REVISION "SDL-release-2.28.0-128-gf032e8c19"
+#define SDL_REVISION "SDL-release-2.28.0-178-g4aab2342e"
 #endif
