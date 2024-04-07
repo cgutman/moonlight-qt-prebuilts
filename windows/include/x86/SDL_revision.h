5e1d46fdd8e0a697dafc197f9c8acb2c4632174d
Cameron Gutman <aicommander@gmail.com>
2024-04-06 19:39:27 -0500

    Update prebuilt libraries
    SDL2 -> 1fa6142
    FFmpeg -> 7.0
    opus -> 1.5.1

--- windows/include/x86/SDL_revision.h
+++ windows/include/x86/SDL_revision.h
@@ -2,7 +2,7 @@
 #define SDL_REVISION_NUMBER 0
 
 #ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL-prerelease-2.29.2-61-gdf00a7dd4 (" SDL_VENDOR_INFO ")"
+#define SDL_REVISION "SDL-prerelease-2.29.2-117-g1fa614290 (" SDL_VENDOR_INFO ")"
 #else
-#define SDL_REVISION "SDL-prerelease-2.29.2-61-gdf00a7dd4"
+#define SDL_REVISION "SDL-prerelease-2.29.2-117-g1fa614290"
 #endif
