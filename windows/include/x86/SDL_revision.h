1b32bfe0a79ef999e6dec832f765c7a7904a731c
Cameron Gutman <aicommander@gmail.com>
2024-07-13 13:20:56 -0500

    Remove 32-bit libraries

--- windows/include/x86/SDL_revision.h
+++
@@ -1,8 +0,0 @@
-/* #undef SDL_VENDOR_INFO */
-#define SDL_REVISION_NUMBER 0
-
-#ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL-prerelease-2.29.2-117-g1fa614290 (" SDL_VENDOR_INFO ")"
-#else
-#define SDL_REVISION "SDL-prerelease-2.29.2-117-g1fa614290"
-#endif
