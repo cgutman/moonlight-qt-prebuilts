a8007d1ef9c5b2a4e5831812b501152387daf8b9
Cameron Gutman <aicommander@gmail.com>
2024-10-20 13:41:20 -0500

    Update prebuilt libraries
    SDL -> 86fd4ed
    FFmpeg -> 7.1 (87ccf995c)
    dav1d -> 1.5.0

--- windows/include/x64/SDL_revision.h
+++ windows/include/x64/SDL_revision.h
@@ -2,7 +2,7 @@
 #define SDL_REVISION_NUMBER 0
 
 #ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL-prerelease-2.29.2-352-g10b4a7937 (" SDL_VENDOR_INFO ")"
+#define SDL_REVISION "SDL-prerelease-2.29.2-401-g86fd4ed83 (" SDL_VENDOR_INFO ")"
 #else
-#define SDL_REVISION "SDL-prerelease-2.29.2-352-g10b4a7937"
+#define SDL_REVISION "SDL-prerelease-2.29.2-401-g86fd4ed83"
 #endif
