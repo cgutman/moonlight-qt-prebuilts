6a5f6036d1aa5515d75753c24e6488615f6aa22e
Cameron Gutman <aicommander@gmail.com>
2026-01-30 19:40:57 -0600

    Switch to SDL3+SDL2-compat
    https://github.com/cgutman/moonlight-deps/commit/269d59a0e4a6763a12096e8f03cdf85cd72473f1

--- windows/include/x64/SDL_revision.h
+++
@@ -1,8 +0,0 @@
-/* #undef SDL_VENDOR_INFO */
-#define SDL_REVISION_NUMBER 0
-
-#ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL-2.33.0-g235e487 (" SDL_VENDOR_INFO ")"
-#else
-#define SDL_REVISION "SDL-2.33.0-g235e487"
-#endif
